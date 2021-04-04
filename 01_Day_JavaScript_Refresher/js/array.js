17f15386a7feb91795cb4d952ffb586967067849
henok <henokmicrometre@gmail.com>
2021-04-04 21:59:57 +0100

    merging

---
+++ 01_Day_JavaScript_Refresher/js/array.js
@@ -0,0 +1,12 @@
+// syntax Using Array constructor
+const constructor = Array()
+// or
+// let arr = new Array()
+console.log(constructor) // []
+// syntax Using square brackets([])
+// This the most recommended way to create an empty list
+const arr = []
+console.log(arr)
+// emty object
+const obj = {}
+console.log(obj)
