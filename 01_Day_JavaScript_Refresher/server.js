c4649d39e2213caba6a94d6de165f9c720ebf021
henok <henokmicrometre@gmail.com>
2021-04-05 09:14:25 +0100

    lesson 1

--- 01_Day_JavaScript_Refresher/server.js
+++ 01_Day_JavaScript_Refresher/server.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-
+var favicon = require('serve-favicon');
 
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public/images', 'favicon.icon')))
+
 
 
 app.get('/', function(req, res) {
