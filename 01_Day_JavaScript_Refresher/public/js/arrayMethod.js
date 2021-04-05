4436c1085d770ee81d15d43e8f93e22514e6ae0f
henok <henokmicrometre@gmail.com>
2021-04-05 19:59:58 +0100

    learnig dom create elemtnt

--- 01_Day_JavaScript_Refresher/public/js/arrayMethod.js
+++ 01_Day_JavaScript_Refresher/public/js/arrayMethod.js
@@ -17,3 +17,16 @@ const eight0values = Array(8).fill(0) // it creates eight element values filled 
 console.log(eight0values) // [0, 0, 0, 0, 0, 0, 0, 0]
 
 
+/*Concatenating array using concat
+concat:To concatenate two arrays.*/
+
+const firstList = [1, 2, 3]
+const secondList = [4, 5, 6]
+const thirdList = firstList.concat(secondList)
+console.log(thirdList) // [1, 2, 3, 4, 5, 6]
+
+const fruits = ['banana', 'orange', 'mango', 'lemon'] // array of fruits
+const vegetables = ['Tomato', 'Potato', 'Cabbage', 'Onion', 'Carrot'] // array of vegetables
+const fruitsAndVegetables = fruits.concat(vegetables) // concatenate the two arrays
+console.log(fruitsAndVegetables)
+
