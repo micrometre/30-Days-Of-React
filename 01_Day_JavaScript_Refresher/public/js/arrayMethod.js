8450255b07fcc8a9e0fae3fa91634c9e448958fa
henok <henokmicrometre@gmail.com>
2021-04-06 16:00:35 +0100

    working with arrays methods

--- 01_Day_JavaScript_Refresher/public/js/arrayMethod.js
+++ 01_Day_JavaScript_Refresher/public/js/arrayMethod.js
@@ -1,14 +1,128 @@
+/*pop: Removing item in the end.
+
+const numbers = [1, 2, 3, 4, 5]
+numbers.pop() // -> remove one item from the end
+
+console.log(numbers) // -> [1,2,3,4]
+
+
+
+
+/*Push: adding item in the end. 
+// syntax
+const arr = ['item1', 'item2', 'item3']
+arr.push('new item')
+console.log(arr)
+
+
+
+
+/*Splice method in array
+
+const numbers = [1, 2, 3, 4, 5]
+console.log(numbers.splice()) // -> remove all items
+console.log(numbers.splice(0, 1)) // remove the first item
+console.log(numbers.splice(3, 3, 7, 8, 9)) // -> [1, 2, 3, 7, 8, 9] //it removes three item and replace three items
+
+
+
+
+
+
+
+/*Slice array elements
+
+const numbers = [1, 2, 3, 4, 5]
+
+console.log(numbers.slice()) // -> it copies all  item
+console.log(numbers.slice(0)) // -> it copies all  item
+console.log(numbers.slice(0, numbers.length)) // it copies all  item
+console.log(numbers.slice(1, 4)) // -> [2,3,4] // it doesn't include the ending position
+
+
+/*join: It is used to join the elements of the array, 
+
+const numbers = [1, 2, 3, 4, 5]
+console.log(numbers.join()) // 1,2,3,4,5
+
+const names = ['Asabeneh', 'Mathias', 'Elias', 'Brook']
+
+console.log(names.join()) // Asabeneh,Mathias,Elias,Brook
+console.log(names.join('')) //AsabenehMathiasEliasBrook
+console.log(names.join(' ')) //Asabeneh Mathias Elias Brook
+console.log(names.join(', ')) //Asabeneh, Mathias, Elias, Brook
+console.log(names.join(' # ')) //Asabeneh # Mathias # Elias # Brook
+
+
+/*toString:Converts array to string
+const numbers = [1, 2, 3, 4, 5]
+console.log(numbers.toString()) // 1,2,3,4,5
+
+const names = ['Asabeneh', 'Mathias', 'Elias', 'Brook']
+console.log(names.toString()) // Asabeneh,Mathias,Elias,Brook
+
+
+
+/*Array.isArray:To check if the data type is an array
+const numbers = [1, 2, 3, 4, 5]
+console.log(Array.isArray(numbers)) // true
+
+const number = 100
+console.log(Array.isArray(number)) // false
+
+
+
+
+/*Check if an item exist in an array. If it exist it returns the true else it returns false.
+const numbers = [1, 2, 3, 4, 5]
+
+console.log(numbers.includes(5)) // true
+console.log(numbers.includes(0)) // false
+console.log(numbers.includes(1)) // true
+console.log(numbers.includes(6)) // false
+
+
+
+
+
+
+
+/*Getting last index of an element in array
+const numbers = [1, 2, 3, 4, 5, 3, 1, 2]
+console.log(numbers.lastIndexOf(2)) // 7
+console.log(numbers.lastIndexOf(0)) // -1
+console.log(numbers.lastIndexOf(1)) //  6
+console.log(numbers.lastIndexOf(4)) //  3
+console.log(numbers.lastIndexOf(6)) // -1
+
+
+
+/*Check an element if it exist in an array.
+const fruits = ['banana', 'orange', 'mango', 'lemon']
+let index = fruits.indexOf('banana') // 0
+
+if (index != -1) {
+  console.log('This fruit does exist in the array')
+} else {
+  console.log('This fruit does not exist in the array')
+}
+
+
+
+
+
+
+
 /*Methods to manipulate array
-Array Constructor*/
 
 const arr = Array() // creates an an empty array
 console.log(arr)
 
 const eightEmptyValues = Array(8) // it creates eight empty values
 console.log(eightEmptyValues) // [empty x 8]
+Array Constructor*/
 
 /*Creating static values with fill
-fill: Fill all the array elements with a static value*/
 
 const eightXvalues = Array(8).fill('X') // it creates eight element values filled with 'X'
 console.log(eightXvalues) // ['X', 'X','X','X','X','X','X','X']
@@ -16,9 +130,9 @@ console.log(eightXvalues) // ['X', 'X','X','X','X','X','X','X']
 const eight0values = Array(8).fill(0) // it creates eight element values filled with '0'
 console.log(eight0values) // [0, 0, 0, 0, 0, 0, 0, 0]
 
+fill: Fill all the array elements with a static value*/
 
 /*Concatenating array using concat
-concat:To concatenate two arrays.*/
 
 const firstList = [1, 2, 3]
 const secondList = [4, 5, 6]
@@ -29,4 +143,23 @@ const fruits = ['banana', 'orange', 'mango', 'lemon'] // array of fruits
 const vegetables = ['Tomato', 'Potato', 'Cabbage', 'Onion', 'Carrot'] // array of vegetables
 const fruitsAndVegetables = fruits.concat(vegetables) // concatenate the two arrays
 console.log(fruitsAndVegetables)
+concat:To concatenate two arrays.*/
+
+
+/*Getting index of an element in an array
+
+console.log(numbers.indexOf(5)) // -> 4
+console.log(numbers.indexOf(0)) // -> -1
+console.log(numbers.indexOf(1)) // -> 0
+console.log(numbers.indexOf(6)) // -> -1
+
+indexOf:To check if an item exist in an array. If it exists it returns the index else it returns -1.*/
+
+
+
+
+
+
+
+
 
